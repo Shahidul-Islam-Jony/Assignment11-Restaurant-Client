f3707fc35b9f9344fe7014207b2a629c5892dc82
Shahidul-Islam <shahidulislam13sf@gmail.com>
2023-11-05 19:14:43 +0600

    User created by email and password implemented

--- src/pages/Registration/Registration.jsx
+++ src/pages/Registration/Registration.jsx
@@ -1,36 +1,122 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { AuthContext } from "../../providers/AuthProvider";
+import { useContext } from "react";
+import swal from 'sweetalert';
 
 const Registration = () => {
+    const { createUser, updateUser, logout } = useContext(AuthContext);
+    const navigate = useNavigate();
+
+    const handleRegistration = e => {
+        e.preventDefault();
+        const form = e.target;
+        const name = form.name.value;
+        const email = form.email.value;
+        const photoUrl = form.photo.value;
+        const password = form.password.value;
+        // console.log(name,email,photoUrl,password);
+        if (password.length < 6) {
+            toast.error('Password should be 6 character or longer!', {
+                position: "top-center",
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "light",
+            });
+            return;
+        }
+        if (!/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,32}$/.test(password)) {
+            toast.error('Password should have atleast one Capital letter one small letter and one special character !', {
+                position: "top-center",
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "light",
+            });
+            return;
+        }
+        // console.log(name,email,photoUrl,password);
+        createUser(email, password)
+            .then(result => {
+                console.log(result);
+                updateUser(name, photoUrl)
+                    .then(() => {
+                        swal("Done!", "Registration successful", "success")
+                        // logout user and navigate to login
+                        logout()
+                            .then(() => { })
+                            .catch(error => console.log(error))
+                        navigate('/login');
+
+                    })
+                    .catch(error => {
+                        toast.error(`${error}`, {
+                            position: "top-center",
+                            autoClose: 5000,
+                            hideProgressBar: false,
+                            closeOnClick: true,
+                            pauseOnHover: true,
+                            draggable: true,
+                            progress: undefined,
+                            theme: "light",
+                        });
+                        return;
+                    })
+
+            })
+            .catch(error => {
+                toast.error(`${error}`, {
+                    position: "top-center",
+                    autoClose: 5000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                    theme: "light",
+                });
+                return;
+            })
+    }
+
     return (
         <div className="mt-20 text-center">
             <div className="flex mx-auto flex-col gap-4 md:flex-row md:px-4">
-                <div className='w-1/2 mx-auto'>
+                <div className='w-full lg:w-1/2 mx-auto'>
                     <h1 className="text-5xl font-bold mb-4">Register now!</h1>
                     <div className="rounded-lg w-full shadow-2xl bg-base-100 px-4 py-6">
-                        <form >
+                        <form onSubmit={handleRegistration}>
                             <div>
                                 <label className="label">
                                     <span className="text-xl font-medium">Name</span>
                                 </label>
-                                <input type="text" placeholder="Your name" className="input rounded-md w-full border-pink-600" required />
+                                <input type="text" name='name' placeholder="Your name" className="input rounded-md w-full border-pink-600" required />
                             </div>
                             <div>
                                 <label className="label">
                                     <span className="text-xl font-medium">Email</span>
                                 </label>
-                                <input type="email" placeholder="Email" className="input rounded-md w-full border-pink-600" required />
+                                <input type="email" name='email' placeholder="Email" className="input rounded-md w-full border-pink-600" required />
                             </div>
                             <div>
                                 <label className="label">
                                     <span className="text-xl font-medium">Photo URL</span>
                                 </label>
-                                <input type="text" placeholder="Please give your photo url" className="input rounded-md w-full border-pink-600" required />
+                                <input type="text" name='photo' placeholder="Please give your photo url" className="input rounded-md w-full border-pink-600" required />
                             </div>
                             <div>
                                 <label className="label">
                                     <span className="text-xl font-medium">Password</span>
                                 </label>
-                                <input type="password" placeholder="password" className="input rounded-md w-full border-pink-600" required />
+                                <input type="password" name='password' placeholder="password" className="input rounded-md w-full border-pink-600" required />
                             </div>
                             <div className="form-control mt-6">
                                 <input className='btn btn-secondary capitalize text-xl font-semibold' type="submit" value="Register" />
@@ -42,6 +128,7 @@ const Registration = () => {
                     </div>
                 </div>
             </div>
+            <ToastContainer></ToastContainer>
         </div>
     );
 };
--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -1,5 +1,5 @@
 import { createContext, useState } from "react";
-import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
+import { createUserWithEmailAndPassword, getAuth, signOut, updateProfile } from "firebase/auth";
 import app from "../firebase/firebase.config";
 import PropTypes from 'prop-types'
 
@@ -10,15 +10,34 @@ const AuthProvider = ({children}) => {
     const [user,setUser] = useState(null);
     const [loading,setLoading] = useState(true);
 
+    // user creating by email and password
     const createUser =(email,password)=>{
         setLoading(true);
         return createUserWithEmailAndPassword(auth,email,password);
+    }
+
+    // update user
+    const updateUser = (name,photoUrl)=>{
+        setLoading(true);
+        return updateProfile(auth.currentUser,{
+            displayName:name, photoURL:photoUrl
+        })
+    }
+
+
+
+    // logout user
+    const logout =()=>{
+        setLoading(true);
+        return signOut(auth);
     }
 
     const authInfo = {
         user,
         loading,
         createUser,
+        updateUser,
+        logout,
     }
     return (
         <AuthContext.Provider value={authInfo}>
