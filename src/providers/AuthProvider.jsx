b52852c1141a56789533bc88df2fd7f9eb42af22
Shahidul-Islam <shahidulislam13sf@gmail.com>
2023-11-05 11:30:11 +0600

    Firebase AuthContext implemented

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -1,10 +1,34 @@
+import { createContext, useState } from "react";
+import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
+import app from "../firebase/firebase.config";
+import PropTypes from 'prop-types'
 
-const AuthProvider = () => {
+export const AuthContext = createContext();
+const auth = getAuth(app);
+
+const AuthProvider = ({children}) => {
+    const [user,setUser] = useState(null);
+    const [loading,setLoading] = useState(true);
+
+    const createUser =(email,password)=>{
+        setLoading(true);
+        return createUserWithEmailAndPassword(auth,email,password);
+    }
+
+    const authInfo = {
+        user,
+        loading,
+        createUser,
+    }
     return (
-        <div>
-            
-        </div>
+        <AuthContext.Provider value={authInfo}>
+            {children}
+        </AuthContext.Provider>
     );
 };
+
+AuthProvider.propTypes = {
+    children:PropTypes.node,
+}
 
 export default AuthProvider;
